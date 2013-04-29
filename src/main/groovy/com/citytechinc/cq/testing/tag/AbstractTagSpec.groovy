f3c5982259520ab6b4e9ef556990e7022e5a3e4c
Mark Daugherty <mark.r.daugherty@gmail.com>
2013-04-29 17:55:16 -0500

    updated spring dependencies for abstract tag spec.

--- src/main/groovy/com/citytechinc/cq/testing/tag/AbstractTagSpec.groovy
+++ src/main/groovy/com/citytechinc/cq/testing/tag/AbstractTagSpec.groovy
@@ -2,46 +2,20 @@ package com.citytechinc.cq.testing.tag
 
 import com.citytechinc.cq.testing.AbstractRepositorySpec
 import org.springframework.mock.web.MockJspWriter
-import spock.lang.Shared
 
 import javax.servlet.jsp.PageContext
+import javax.servlet.jsp.tagext.TagSupport
 
 abstract class AbstractTagSpec extends AbstractRepositorySpec {
 
-    /**
-     * Tag instance is shared, but instantiation is delegated to the concrete test class.
-     */
-    @Shared tag
-
-    @Shared writer
-
-    /**
-     * Instantiate the tag class being tested.
-     */
-    abstract void createTag()
-
-    /**
-     * Before each test, create a new writer and tag instance.
-     */
-    def setup() {
-        writer = new StringWriter()
-
-        createTag()
-    }
-
-    def mockPageContext() {
+    def mockPageContext(TagSupport tag, StringWriter writer) {
         def pageContext = Mock(PageContext)
         def jspWriter = new MockJspWriter(writer)
 
         pageContext.out >> jspWriter
 
+        tag.pageContext = pageContext
+
         pageContext
-    }
-
-    def getOutput() {
-        tag.doStartTag()
-        tag.doEndTag()
-
-        writer.toString()
     }
 }
