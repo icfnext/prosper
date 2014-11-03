85f1ba6e6377c878c98c20822b43faeb6b3a771c
Mark Daugherty <mark.r.daugherty@gmail.com>
2014-11-03 13:49:31 -0600

    updated request builder to fix mock delegate issue.

--- src/main/groovy/com/citytechinc/aem/prosper/builders/BindingsBuilder.groovy
+++ src/main/groovy/com/citytechinc/aem/prosper/builders/BindingsBuilder.groovy
@@ -33,7 +33,7 @@ class BindingsBuilder {
     private final RequestBuilder requestBuilder
 
     @Delegate
-    private final ResponseBuilder responseBuilder = new ResponseBuilder()
+    private final ResponseBuilder responseBuilder
 
     private final def services = [:]
 
@@ -66,6 +66,7 @@ class BindingsBuilder {
         this.resourceResolver = resourceResolver
 
         requestBuilder = new RequestBuilder(resourceResolver)
+        responseBuilder = new ResponseBuilder()
     }
 
     /**
